f5a6388f1cb9fb0c4b583601516354f129729426
Tantsevov Grigory <tancevov.g@wildberries.ru>
2019-12-24 18:19:33 +0300

    cicle refactor

--- MPI-Version/main.cpp
+++ MPI-Version/main.cpp
@@ -67,10 +67,16 @@ int main(int argc, char **argv)
 
 
     // FFT
-    double starttime=MPI_Wtime();
+    double total = 0;
+    double totalBCast = 0;
+    double totalRecv = 0;
+    double totalLast = 0;
 
     for (int iter = 0; iter < numTries; iter++) {
+        double startBCast = MPI_Wtime();
         MPI_Bcast(&polynomial.front(), polynomial.size(), MPI_C_DOUBLE_COMPLEX, 0, MPI_COMM_WORLD);
+        double endBCast = MPI_Wtime();
+        totalBCast += (endBCast - startBCast) * 1000;
 
         int n = static_cast<int>(polynomial.size());
         int lg_n = 0;
@@ -85,35 +91,35 @@ int main(int argc, char **argv)
         int degreeOfPolynomial = ceil(log2(n));
         int numParalleIterations = degreeOfPolynomial - ceil(log2(size));
         int partSize = n / size;
-
+        
+        double starttime=MPI_Wtime();
         for (int z = 0; z < numParalleIterations; ++z) {
             int len = pow(2, z + 1);
             double ang = 2 * M_PI / len;
             base wlen(cos(ang), sin(ang));
-            for (int i = 0; i < n; i += len) {
-                base w(1);
-                for (int j = 0; j < len / 2; ++j) {
-                    int first = i+j, second = i + j + len / 2;
-                    if ((partSize*rank <= first < partSize*(rank+1)) && (partSize*rank <= second < partSize*(rank+1))) {
-                        base u = polynomial[first], v = polynomial[second] * w;
-                        polynomial[first] = u + v;
-                        polynomial[second] = u - v;
-                    }
-
-                    w *= wlen;
+            base w(1);
+            for (int offset = 1; offset < partSize; offset *= 2) {
+                for (int i = partSize*rank; i < partSize / 2; i++) {
+                    base u = polynomial[i], v = polynomial[i + offset] * w;
+                    polynomial[i] = u + v;
+                    polynomial[i+offset] = u - v;
                 }
             }
         }
 
         if (rank == 0) {
+            double startRecv = MPI_Wtime();
             for (int i = 1; i < size; i++) {
                 MPI_Recv(&polynomial[i*partSize], partSize, MPI_C_DOUBLE_COMPLEX, i, 42, MPI_COMM_WORLD, &status);
             }
+            double endRecv = MPI_Wtime();
+            totalRecv += (endRecv - startRecv) * 1000;
         } else {
             MPI_Send(&polynomial[rank*partSize], partSize, MPI_C_DOUBLE_COMPLEX, 0, 42, MPI_COMM_WORLD);
         }
 
         if (rank == 0) {
+            double startLast = MPI_Wtime();
             for (int z = numParalleIterations; z < degreeOfPolynomial; ++z) {
                 int len = pow(2, z + 1);
                 double ang = 2 * M_PI / len;
@@ -128,12 +134,18 @@ int main(int argc, char **argv)
                     }
                 }
             }
+            double endLast = MPI_Wtime();
+            totalLast += (endLast - startLast) * 1000;
         } 
+        double endtime = MPI_Wtime();
+        total += (endtime - starttime) * 1000;
     }
-    double endtime = MPI_Wtime();
     if (rank == 0) {
         std::cout << "Average time ms for " << numTries << " tries ";
-        std::cout << 1.0 * (endtime-starttime)*1000 / (1.0 * numTries) << "ms" << std::endl;
+        std::cout << total / (1.0 * numTries) << "ms" << std::endl;
+        std::cout << "Average BCast: " << totalBCast / (1.0 * numTries) << "ms" << std::endl;
+        std::cout << "Average Recv: " << totalRecv / (1.0 * numTries) << "ms" << std::endl;
+        std::cout << "Average Last: " << totalLast / (1.0 * numTries) << "ms" << endl;
     }
     MPI_Finalize();
     return 0;
